10621ad41cbcc9fadee7acb6a732fd2b1fdcaa05
Thompson Manda <thompsonmanda08@outlook.com>
2023-04-24 05:01:20 +0200

    fix import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import { TodoListContext } from "~/context/todoListContext";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
